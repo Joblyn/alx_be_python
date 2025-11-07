4b931e9b3fcd5c77b06b2a27e6431a4e16c538f7
Job Oaikhenah <oaikhenahjob@gmail.com>
2025-11-07 23:37:34 +0100

    hours to seconds

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,8 @@
+# This program converts hours to seconds.
+# Formula: S = H * 3600
+# ( S ) is the time in seconds,
+# ( H ) is the time in hours.
+
+hours = 2  # time in hours
+seconds = hours * 3600
+print(hours, "hours", "in seconds is:", seconds, "seconds.")
